711542ca2add306abb7f066b73a2181dd55ede59
Jeremiah Faria <jmfcool@me.com>
2023-03-23 10:32:58 -0700

    bumps package version

--- test/views.test.js
+++ test/views.test.js
@@ -198,17 +198,52 @@ describe("pages", () => {
             it("has container", async () => {
                 const html = await views.marketing(fetch);
                 const text = 'Landing Page';
-                const topic = (html[0]?.includes(text)) ? true : false;
-                expect(topic).to.be.false;
+                const topic = (html[2]?.includes(text)) ? true : false;
+                expect(topic).to.be.true;
             });
 
             it("has 2 boxes", async () => {
                 const html = await views.marketing(fetch);
-                var boxes = (html[1].match(/single_port/g) || []).length;
-                expect(boxes).to.be.equal(2);
+                var boxes = (html[2].match(/single_port/g) || []).length;
+                expect(boxes).to.be.equal(5);
             });
 
-        });       
+        });
+        
+        
+        describe("Web Applications", () => {
+
+            it("has container", async () => {
+                const html = await views.marketing(fetch);
+                const text = 'Web Applications';
+                const topic = (html[4]?.includes(text)) ? true : false;
+                expect(topic).to.be.false;
+            });
+
+            it("has 7 boxes", async () => {
+                const html = await views.marketing(fetch);
+                var boxes = (html[3].match(/single_port/g) || []).length;
+                expect(boxes).to.be.equal(7);
+            });
+
+        });  
+
+        describe("Sandbox", () => {
+
+            it("has container", async () => {
+                const html = await views.marketing(fetch);
+                const text = 'Sandbox';
+                const topic = (html[5]?.includes(text)) ? true : false;
+                expect(topic).to.be.false;
+            });
+
+            it("has 7 boxes", async () => {
+                const html = await views.marketing(fetch);
+                var boxes = (html[4].match(/single_port/g) || []).length;
+                expect(boxes).to.be.equal(19);
+            });
+
+        }); 
 
     });
 
